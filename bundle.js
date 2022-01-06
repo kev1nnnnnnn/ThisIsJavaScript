012b221fabcd9b289938d66812421362324b3eb2
John Kevin <60278232+kev1nnnnnnn@users.noreply.github.com>
2022-01-06 16:06:03 -0400

    BABEL
    tolling EC6

---
+++ bundle.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Pessoa = /*#__PURE__*/_createClass(function Pessoa(nome, sobrenome) {
+  _classCallCheck(this, Pessoa);
+
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+});
